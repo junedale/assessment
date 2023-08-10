eaae55d0e9f76ffe381396abf820a041e017b9cd
junedale <55942587+junedale@users.noreply.github.com>
2023-08-10 18:12:57 +0800

    Update process.js

--- backend/routes/process.js
+++ backend/routes/process.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const processController = require('../controllers/processController');
 
 router.post('/original', [
-    body('anytext').isURL().withMessage('Please enter a valid URL')
+    body('anytext').isURL().withMessage('Please enter a valid URL').isEmpty().withMessage('Please enter a URL')
 ], processController.postOriginal);
 
 
